cb236cf361b6d998ec8c80888d452a5e44f07a67
Javier Junquera Sánchez <javier@junquera.xyz>
2019-02-21 08:57:42 +0100

    Building command and minor refactor

--- moneroRpcClass.py
+++ moneroRpcClass.py
@@ -1,10 +1,37 @@
 import requests
+import random
 
 class MoneroRPC:
 
     # monero-wallet-rpc.exe --stagenet --password "" --rpc-bind-port 28088 --disable-rpc-login  --daemon-host monero-stagenet.exan.tech:38081  --wallet-dir C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet
-    #TODO 
+    #TODO
     #init method and start monero-wallet-rpc
+
+    def __init__(self, wallet_dir):
+
+        self._proxy_host = "127.0.0.1"
+        self._proxy_port = random.randint(10240, 65000) # Puerto aleatorio para poder lanzar varios
+
+        command = ["monero-wallet-rpc.exe"]
+        options = dict(
+            disable_login = '--disable-rpc-login',
+            net = '--stagenet',
+            password = '--password ""',
+            rpc_port = '--rpc-bind-port %d' % (self._proxy_port),
+            daemon_host = '--daemon-host monero-stagenet.exan.tech:38081',
+            wallet_dit = '--wallet-dir "%s"' % wallet_dir # C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet
+        )
+
+        command += ["%s" % v for v in options.values()]
+
+    @property
+    def proxy_address(self):
+        addr = "http://%s:%d" % (self._proxy_host, self._proxy_port)
+        return addr
+
+    @property
+    def json_rpc_address(self):
+        return "%s/json_rpc" % self.proxy_address
 
     def getAddress(self):
 
@@ -14,7 +41,7 @@ class MoneroRPC:
 
         data = '{"jsonrpc":"2.0","id":"0","method":"get_address","params":{"account_index":0}}'
 
-        response = requests.post('http://127.0.0.1:28088/json_rpc', headers=headers, data=data)
+        response = requests.post(self.json_rpc_address, headers=headers, data=data)
 
         return response.json()['result']
 
@@ -26,7 +53,7 @@ class MoneroRPC:
 
         data = '{"jsonrpc":"2.0","id":"0","method":"get_transfers","params":{"out":true}}'
 
-        response = requests.post('http://127.0.0.1:28088/json_rpc', headers=headers, data=data)
+        response = requests.post(self.json_rpc_address, headers=headers, data=data)
 
         return response.json()['result']['out']
 
@@ -38,7 +65,7 @@ class MoneroRPC:
 
         data = '{"jsonrpc":"2.0","id":"0","method":"get_transfers","params":{"in":true}}'
 
-        response = requests.post('http://127.0.0.1:28088/json_rpc', headers=headers, data=data)
+        response = requests.post(self.json_rpc_address, headers=headers, data=data)
 
         return response.json()['result']['in']
 
@@ -47,7 +74,7 @@ class MoneroRPC:
 
 
 
+#
+# moneroRPC = MoneroRPC()
 
-moneroRPC = MoneroRPC()
-
-moneroRPC.getAddress()
+# moneroRPC.getAddress()
