8f36fad810e44cb6d5f151fa259cdbd83ca4348f
Carlos <carlos.cilleruelo@alphasec.xyz>
2019-02-21 09:09:13 +0100

    Update moneroRpcClass.py
    Solve bug launching monero-rpc server

--- moneroRpcClass.py
+++ moneroRpcClass.py
@@ -1,32 +1,33 @@
 import requests
 import random
 import subprocess
+import time
 
 class MoneroRPC:
 
-    # monero-wallet-rpc.exe --stagenet --password "" --rpc-bind-port 28088 --disable-rpc-login  --daemon-host monero-stagenet.exan.tech:38081  --wallet-dir C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet
-    #TODO
-    #init method and start monero-wallet-rpc
+    #monero-wallet-rpc.exe --stagenet --password "" --rpc-bind-port 28088 --disable-rpc-login  --daemon-host monero-stagenet.exan.tech:38081  --wallet-file C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet\carlos-stagenet#init method and start monero-wallet-rpc
 
-    def __init__(self, wallet_dir):
+    def __init__(self, wallet_file):
 
         self._proxy_host = "127.0.0.1"
         self._proxy_port = random.randint(10240, 65000) # Puerto aleatorio para poder lanzar varios
 
-        command = ["monero-wallet-rpc.exe"]
+        command = ["C:\\Users\\carlos\\Desktop\\monero-gui-v0.13.0.4\\monero-wallet-rpc.exe"]
         options = dict(
             disable_login = '--disable-rpc-login',
             net = '--stagenet',
             password = '--password ""',
             rpc_port = '--rpc-bind-port %d' % (self._proxy_port),
             daemon_host = '--daemon-host monero-stagenet.exan.tech:38081',
-            wallet_dit = '--wallet-dir "%s"' % wallet_dir # C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet
+            wallet_dit = '--wallet-file "%s"' % wallet_file # C:\Users\carlos\Documents\Monero\wallets\carlos-stagenet
         )
 
         command += ["%s" % v for v in options.values()]
 
         # TODO Ver output
-        self._p = subprocess.Popen(command)
+        print(" ".join(command))
+        self._p = subprocess.Popen(" ".join(command))
+        time.sleep(5)
 
     # TODO Implementar bien salida
     def __exit__(self):
@@ -52,6 +53,18 @@ class MoneroRPC:
         response = requests.post(self.json_rpc_address, headers=headers, data=data)
 
         return response.json()['result']
+
+    def getBalance(self):
+
+        headers = {
+        'Content-Type': 'application/json',
+        }
+        
+        data = '{"jsonrpc":"2.0","id":"0","method":"get_balance","params":{"account_index":0,"address_indices":[0,1]}}' 
+
+        response = requests.post(self.json_rpc_address, headers=headers, data=data)
+
+        return response.json()
 
     def getOutTransfers(self):
 
@@ -82,7 +95,9 @@ class MoneroRPC:
 
 
 
-#
-# moneroRPC = MoneroRPC()
 
-# moneroRPC.getAddress()
+
+#
+moneroRPC = MoneroRPC('C:\\Users\\carlos\\Documents\\Monero\\wallets\\carlos-stagenet\\carlos-stagenet')
+
+print(moneroRPC.getBalance())
